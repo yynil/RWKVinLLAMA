71c630e65d7c9699e1034b31f190cf8bb1d2214d
yynil <yueyu.lin@me.com>
2024-09-30 03:26:02 +0000

    add max_model_len in args

--- train_scripts/create_vllm_servers.sh
+++ train_scripts/create_vllm_servers.sh
@@ -2,7 +2,8 @@
 NUM_DEVICES=8
 MODEL_ID="/data/rwkv/models/meta-llama/Meta-Llama-3.1-8B-Instruct/"
 BASE_PORT=8001
-while getopts ":n:m:p:" opt; do
+MAX_MODEL_LEN=16384
+while getopts ":n:m:p:l:" opt; do
     case $opt in
         n) NUM_DEVICES="$OPTARG"
         ;;
@@ -10,10 +11,12 @@ while getopts ":n:m:p:" opt; do
         ;;
         p) BASE_PORT="$OPTARG"
         ;;
+        l) MAX_MODEL_LEN="$OPTARG"
+        ;;  
     esac
 done
 
 for i in $(seq 0 $((NUM_DEVICES-1))); do
     port=$((BASE_PORT + i))
-    CUDA_VISIBLE_DEVICES=$i vllm serve ${MODEL_ID} --port ${port} --enable-chunked-prefill --pipeline-parallel-size 1 --enable_prefix_caching &
+    CUDA_VISIBLE_DEVICES=$i vllm serve ${MODEL_ID} --max-model-len ${MAX_MODEL_LEN} --port ${port} --enable-chunked-prefill --pipeline-parallel-size 1 --enable_prefix_caching &
 done
